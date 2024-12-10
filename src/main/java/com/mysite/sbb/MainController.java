a197da54efdfe712a3b28f669fd8fc4dd1741fbb
Nicholas070 <kid0807@naver.com>
2024-12-10 11:53:38 +0900

    no action method name must be readable

---
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -0,0 +1,28 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public void home() {
+        System.out.println("home");
+        this.contact();
+    }
+
+    @GetMapping("/about")
+    public void about() {
+        System.out.println("about");
+        this.contact();
+    }
+
+    public void contact() {
+        System.out.println("contact");
+    }
+}
+    //혹시 무슨 차이가 있었떤 건가요?
+    //기존의 자바 클래스나 파일들은 실행할때 의존성이 없어서 각각의 파일과 클래스별로 실행이 되었지만.
+    //스프링 부트는 프로그램의 진입점이 SbbApplication으로 정해져있어서 그렇습니다.
+    //테스트 하실려면 저친구를 실행시켜주셔야됩니다 넵감사합니다
